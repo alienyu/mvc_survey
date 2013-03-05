de0ee8e23a6263fdfee07f732f4e008a57f8b03a
Herbert Shi <351096578@qq.com>
2013-03-05 21:32:56 +0800

    add change event in baseinfo.js

--- js/controller/baseinfo.js
+++ js/controller/baseinfo.js
@@ -1,13 +1,31 @@
 var BaseInfo = Spine.Controller.sub({
-  template: function () {
-    return $("#base-info-template").html();
-  },
-
-  show: function () {
-    this.el.html(this.template());
-  },
-
-  init: function() {
-    this.show();
-  }
+    template: function () {
+        return $("#base-info-template").html();
+    },
+    events: {
+        "change input[type=file]": "preImg"
+    },
+    show: function () {
+        this.el.html(this.template());
+    },
+    init: function () {
+        //show template
+        this.show();
+        //init widgets
+        $(".end-time").datepicker();
+    },
+    preImg: function (e) {
+        var sourceId = $(e.target).attr("id");
+        var targetId = sourceId.replace("upload", "imgPre");
+        if (typeof FileReader === 'undefined') {
+            alert('Your browser does not support FileReader...');
+            return;
+        }
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var img = document.getElementById(targetId);
+            img.src = this.result;
+        }
+        reader.readAsDataURL(document.getElementById(sourceId).files[0]);
+    }
 });

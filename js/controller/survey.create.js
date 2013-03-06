41c0e319aae7bbbd6c1947fdc2894c8a974b06b4
charleschu <cuxang@gmail.com>
2013-03-06 17:11:03 +0800

    add survey creator controller methods

--- js/controller/survey.create.js
+++ js/controller/survey.create.js
@@ -3,23 +3,45 @@ var SurveyCreate = Spine.Controller.sub({
     template: function () {
         return $("#survey-create-template").html();
     },
-    //events: {"":""},
+
+    elements: {
+      "#creator-area": "creatorArea"
+    },
+
+    events: {
+      "click #add-option-tag": "addOption",
+      "click .remove-option-tag": "removeOption"},
+
     show: function () {
         this.el.html(this.template());
     },
-    addQuestion: function () {
+
+    initRadioCreator: function () {
+      this.questionTextCreator();
+      var optionCreatorTemp = $($("#radio-option-creator-template").html()).tmpl({"optionTag": "A"});
+      $(this.creatorArea).append(optionCreatorTemp);
+    },
+
+    questionTextCreator: function () {
+      var questionTextCreator = $("#question-text-creator-template").html();
+      $(this.creatorArea).append(questionTextCreator);
+      $("#question-text").wysiwyg();
+    },
+
+    bindDraggable: function () {
+        var that = this;
         $("#question-tool-bar div").each(function (index, element) {
             $(element).draggable({ opacity: 0.7, helper: "clone", cursor: "move" });
         });
 
         //add question
-        $("#question-template").droppable({
+        $("#creator-area").droppable({
             drop: function (e, ui) {
-                $("#question-template").empty();
+                $("#creator-area").empty();
                 var drag = ui.draggable.attr("id");
                 switch (drag) {
                     case "single-select":
-                        //TODO:create single select question
+                        that.initRadioCreator();
                         break;
                     case "multi-select":
                         //TODO:create multi select question
@@ -37,14 +59,26 @@ var SurveyCreate = Spine.Controller.sub({
             }
         });
     },
+
     init: function () {
         //show template
         this.show();
-        //init widgets
-        Spine.trigger("show_control_bar", this.el);
-        //add question by drag
-        this.addQuestion();
+        //inital the question creator draggable
+        this.bindDraggable();
     },
-    _sortQuestion: function () {
+
+    addOption: function () {
+        var indexTag = String.fromCharCode(65 + $("#option-creators .option-creator").size());
+        var optionCreatorTemp = $($("#radio-option-creator-template").html())
+                                .find("#option-creators").tmpl({"optionTag": indexTag});
+        $("#option-creators").append(optionCreatorTemp);
+    },
+
+    removeOption: function (e) {
+      e.target.parentElement.remove();
+      var optionCreators = $('#option-creators .option-creator');
+      optionCreators.each(function (item, element) {
+          $(element).find("span").html((String.fromCharCode(65 + item)));
+      });
     }
 });

73712e9a64f1384b012a5d2a80114531788df737
Herbert Shi <351096578@qq.com>
2013-03-06 21:35:49 +0800

    mockup "survey-preview-template"

--- js/controller/question.preview.js
+++ js/controller/question.preview.js
@@ -1,26 +1,30 @@
 // this is for the question preview
 var QuestionPreview = Spine.Controller.sub({
-  template: function () {
-
-  },
-  show: function () {
-  },
-  init: function () {
-    //show template
-    this.show();
-    this.sortableList();
-    //init widgets
-  },
-  sortableList: function () {
-    $('#ul_serveyList').sortable({
-      containment: "parent",
-      items: "li:not(.question_editor)",
-      deactivate: function (event, ui) {
-        $("#ul_serveyList li").each(function (index, element) {
-          $(element).find(".view-question-content").children("span").html(index + 1);
+    template: function () {
+        return $("#survey-preview-template").html();
+    },
+    elements: {
+        "#survey-preview-list": "surveyPreviewList"
+    },
+    show: function () {
+        this.el.html(this.template());
+    },
+    init: function () {
+        //show template
+        this.show();
+        this.sortableList();
+        //init widgets
+    },
+    sortableList: function () {
+        $(this.surveyPreviewList).sortable({
+            containment: "parent",
+            items: "li",
+            deactivate: function (event, ui) {
+                ui.item.parent().find("li").each(function (index, element) {
+                    $(element).find(".view-question-content").children("span").html(index + 1);
+                });
+            }
         });
-      }
-    });
-    $('#ul_serveyList').disableSelection();
-  }
+        $(this.surveyPreviewList).disableSelection();
+    }
 });

231e07c71e92038cf8c4608d3ccc6d3cef25d346
charleschu <cuxang@gmail.com>
2013-03-06 14:47:52 +0800

    fix some id

--- js/index.js
+++ js/index.js
@@ -7,11 +7,8 @@ $(function () {
 
             this.baseinfo = new BaseInfo({ el: $("#add-base-info") });
             this.surveycreate = new SurveyCreate({ el: $("#build-survey-questions") });
-          this.questionPreview = new QuestionPreview({el: $("#survey_preview")});
+            this.questionPreview = new QuestionPreview({el: $("#survey-preview")});
 
-        },
-        showControlBar: function (obj) {
-            alert($(obj).index());
         }
     });
 

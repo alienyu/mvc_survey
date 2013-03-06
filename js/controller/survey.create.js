d2a9bab5b78a9557bca7c485d637b4255bfa828d
Herbert Shi <351096578@qq.com>
2013-03-06 15:29:41 +0800

    modified way of grag init

--- js/controller/survey.create.js
+++ js/controller/survey.create.js
@@ -1,53 +1,50 @@
 // this is for the survey create page
 var SurveyCreate = Spine.Controller.sub({
-  template: function () {
-    return $("#survey-create-template").html();
-  },
-//events: {"":""},
-  show: function () {
-    this.el.html(this.template());
-  },
-  addQuestion :function(){
-      //add dragable
-      $("#single_select").draggable({ opacity: 0.7, helper: "clone", cursor: "move" });
-      $("#multi_select").draggable({ opacity: 0.7, helper: "clone" });
-      $("#matrix").draggable({ opacity: 0.7, helper: "clone" });
-      $("#open").draggable({ opacity: 0.7, helper: "clone" });
-      $("#area").draggable({ opacity: 0.7, helper: "clone" });
+    template: function () {
+        return $("#survey-create-template").html();
+    },
+    //events: {"":""},
+    show: function () {
+        this.el.html(this.template());
+    },
+    addQuestion: function () {
+        $("#question-tool-bar div").each(function (index, element) {
+            $(element).draggable({ opacity: 0.7, helper: "clone", cursor: "move" });
+        });
 
-      //add question
-      $("#question-template").droppable({
-          drop: function (e, ui) {
-              $('#question-template').empty();
-              var drag = ui.draggable.text().trim();
-              switch (drag) {
-                  case '单选':
-                      //TODO:create single select question
-                      break;
-                  case '多选':
-                      //TODO:create multi select question
-                      break;
-                  case '矩阵':
-                      //TODO:create matrix question
-                      break;
-                  case '开放':
-                      //TODO:create open question
-                      break;
-                  case '地区':
-                      //TODO:create region question
-                      break;
-              }
-          }
-      });
-  },
-  init: function () {
-    //show template
-    this.show();
-    //init widgets
-    Spine.trigger("show_control_bar", this.el);
-    //add question by drag
-    this.addQuestion();
-  },
-  _sortQuestion: function () {
-  }
+        //add question
+        $("#question-template").droppable({
+            drop: function (e, ui) {
+                $("#question-template").empty();
+                var drag = ui.draggable.attr("id");
+                switch (drag) {
+                    case "single-select":
+                        //TODO:create single select question
+                        break;
+                    case "multi-select":
+                        //TODO:create multi select question
+                        break;
+                    case "matrix":
+                        //TODO:create matrix question
+                        break;
+                    case "open":
+                        //TODO:create open question
+                        break;
+                    case "area":
+                        //TODO:create region question
+                        break;
+                }
+            }
+        });
+    },
+    init: function () {
+        //show template
+        this.show();
+        //init widgets
+        Spine.trigger("show_control_bar", this.el);
+        //add question by drag
+        this.addQuestion();
+    },
+    _sortQuestion: function () {
+    }
 });

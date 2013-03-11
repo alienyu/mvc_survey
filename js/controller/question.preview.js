761633861d29027d9607630ccc3688cd53d60e00
Herbert Shi <351096578@qq.com>
2013-03-11 11:48:30 +0800

    fix conflict

--- js/controller/question.preview.js
+++ js/controller/question.preview.js
@@ -50,8 +50,9 @@ var QuestionPreview = Spine.Controller.sub({
                 radioValue.content = element.content;
                 $("#radio-option-template").tmpl(radioValue).appendTo(".option-list:last");
             }
-            else {
-                $("#open-option-template").tmpl({ "index": element.index }).appendTo(".option-list:last");
+            else
+            {
+                $("#open-option-template").tmpl({"index": element.index}).appendTo(".option-list:last");
             }
         });
 
@@ -61,15 +62,16 @@ var QuestionPreview = Spine.Controller.sub({
         var optionsValue = [];
         var questionOptions = '';
         $(options).each(function (index, element) {
-            if (element.type === "选项") {
+            if (element.type ==="选项") {
                 var multiValue = {};
                 multiValue.name = questionTag;
                 multiValue.index = element.index;
                 multiValue.content = element.content;
                 $("#multi-option-template").tmpl(multiValue).appendTo(".option-list:last");
             }
-            else {
-                $("#open-option-template").tmpl({ "index": element.index }).appendTo(".option-list:last");
+            else
+            {
+                $("#open-option-template").tmpl({"index": element.index}).appendTo(".option-list:last");
             }
         });
 
@@ -117,9 +119,5 @@ var QuestionPreview = Spine.Controller.sub({
         var index = $(e.target).parents("li").index();
         $('#tabs').tabs('option', 'active', 1);
         Spine.trigger("clickEdit", index);
-    },
-
-    submitSurvey: function () {
-        surveyInstance.submitSurvey();
     }
 });

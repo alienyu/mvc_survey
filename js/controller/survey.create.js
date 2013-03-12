3d91fa0aa9a1f5af1cbbbe7f466daf40ae62e2f6
Jack Ma <supermaty@126.com>
2013-03-12 10:59:01 +0800

    fix bug
    fix bug when no area checkbox is checked

--- js/controller/survey.create.js
+++ js/controller/survey.create.js
@@ -192,7 +192,9 @@ var SurveyCreate = Spine.Controller.sub({
 
     getArea: function () {
         //return the last checked checkbox id
-        return $("#areaType").find("input:checked").toArray().pop().id;
+        if($("#areaType").find("input:checked").toArray().pop()){
+            return $("#areaType").find("input:checked").toArray().pop().id;
+        }
     },
 
     changeSelectionView: function (e) {

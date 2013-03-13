f5af8fb99ea8be0ed8512ea05a4a23cbdf91d92b
vivianyyf <vivianyyf@foxmail.com>
2013-03-13 14:41:14 +0800

    fix conflict

---
+++ js/controller/survey.do.js
@@ -0,0 +1,152 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ChuwaJack
+ * Date: 13-3-13
+ * Time: 上午10:31
+ * To change this template use File | Settings | File Templates.
+ */
+var SurveyDo = Spine.Controller.sub({
+    elements: {
+        "#page_cont": "page_cont"
+    },
+
+    events: {
+        "click #save-answer": "saveAnswer",
+        "change ul dl dd select": "areaSelectChange"
+
+    },
+
+    show: function () {
+        $(this.page_cont).html(json.question_html);
+    },
+
+    init: function () {
+        // page step
+        $("#main>div").hide();
+        $($("#main>div")[0]).show();
+        $(".page_next").click(function () {
+            $("#main>div").hide();
+            $(this).parents(".paper_next_container").next().show();
+        });
+        $("#page_cont").find("div").remove();
+        this.show();
+    },
+
+    areaSelectChange: function (e) {
+        var options = "";
+        switch ($(e.target).next().attr('class')) {
+            case "province" :
+                for (var i = 0; i < data_province.length; i++){
+                    var option = "<option value='" + data_province[i].code + "'>" + data_province[i].name + "</option>";
+                    options += option;
+                }
+                break;
+            case "city":
+                for (var i = 0; i < data_city.length; i++){
+                    if(data_city[i].code.substring(0,2) === e.target.value.substring(0,2)) {
+                        var option = "<option value='" + data_city[i].code + "'>" + data_city[i].name + "</option>";
+                        options += option;
+                    }
+                }
+                break;
+            case  "district":
+                for (var i = 0; i < data_district.length; i++){
+                    if(data_district[i].code.substring(0,4) === e.target.value.substring(0,4)) {
+                        var option = "<option value='" + data_district[i].code + "'>" + data_district[i].name + "</option>";
+                        options += option;
+                    }
+                }
+                break;
+            default :
+                break;
+        }
+        $(e.target.nextElementSibling).empty().append("<option>请选择</option>" + options );
+    },
+
+    saveAnswer: function() {
+        var answerlist = [];
+        var aa = [];
+        $(json.topic_list).each(function (index, element) {
+            var obj = $('#page_cont>ul').find('dl')[index];
+            var question_no = element.question_no;
+            var question_type = element.question_type;
+            var answer_detail_list = [];
+            switch (question_type) {
+                case "0":
+                case "1":
+                    $(obj).find('dd>input').each(function (i, e) {
+                        if (e.checked === true) {
+                            if ($(e).parent().find('textarea')) {
+                                answer_detail_list.push({
+                                    question_value: element.options[i].item_num,
+                                    open_question_value: $(e).parent().find('textarea').val(),
+                                    province: "",
+                                    city: "",
+                                    area: ""
+                                });
+                            }
+                            else {
+                                answer_detail_list.push({
+                                    question_value: element.options[i].item_num,
+                                    open_question_value: "",
+                                    province: "",
+                                    city: "",
+                                    area: ""
+                                });
+                            }
+                        }
+                    });
+                    break;
+                case "3":
+                    var bb = "";
+                    if ($(obj).find('textarea').val()) {
+                        bb = $(obj).find('textarea').val();
+                    }
+                    else {
+                        bb = $(obj).find('input').val();
+                    }
+                    answer_detail_list.push({
+                        question_value: "",
+                        open_question_value: bb,
+                        province: "",
+                        city: "",
+                        area: ""
+                    });
+                    break;
+                case "4":
+                    answer_detail_list.push({
+                        question_value: "",
+                        open_question_value: "",
+                        province: $(obj).find('.province').val(),
+                        city: $(obj).find('.city').val(),
+                        area: $(obj).find('.district').val()
+                    });
+                    break;
+            };
+            aa.push({
+                result_id: json.result_id, //--->result_id,
+                question_no: question_no,
+                question_type: question_type,
+                answer_detail_list: answer_detail_list
+            });
+        });
+
+        var answer = {
+            exam_id: "513efa775558883fbc56ce3c",
+            start_time:"11/3/2013",
+            end_time: "11/3/2013",
+            is_effective: "Y",//('Y':有效，'N':无效,(默认'Y'))
+            email: json.email,
+            answer_list:aa
+        };
+        console.log(JSON.stringify(answer));
+        $.ajax({
+            url: "/surveyPlatform/examination/saveAnswer",
+            type: "POST",
+            data: JSON.stringify(answer),
+            success: function (response, option) {
+
+            }
+        });
+    }
+});

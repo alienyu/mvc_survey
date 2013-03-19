44736ff6231782d47936c97230e36df4019d4dc6
alienyu <alienyu@foxmail.com>
2013-03-19 10:00:49 +0800

    refactor

--- js/controller/baseinfo.js
+++ js/controller/baseinfo.js
@@ -1,11 +1,10 @@
-var isValid = 0;
 var BaseInfo = Spine.Controller.sub({
     template: function () {
         return $("#base-info-template").html();
     },
     events: {
         "change input[type=file]": "preImg",
-        "click .next_step": "baseInfoValid"
+        "click .next_step": "nextPage"
     },
     show: function () {
         this.el.html(this.template());
@@ -21,6 +20,16 @@ var BaseInfo = Spine.Controller.sub({
         var targetId = sourceId.replace("upload", "imgPre");
         imagePreview(sourceId, targetId);
     },
+
+    nextPage: function() {
+      this.baseInfoValid();
+      if (isValid == 1) {return}
+      $('.survey_name').find('div>span').text($('#question-name').val());
+      //turn to next page
+      $('#tabs').tabs('enable', 1);
+      $('#tabs').tabs('option', 'active', 1 );
+    },
+
     baseInfoValid: function() {
         isValid = 0;
         //valid text type info
@@ -28,11 +37,13 @@ var BaseInfo = Spine.Controller.sub({
             if (!($(element).val())) {
                 alert($(element.parentNode).find('label').text() + "不能为空");
                 isValid = 1;
+                return false;
             }
             else {
                 if ((element.id === "join-point" && !(/^[1-9]\d*$/.test($(element).val())))|| (element.id === "complete-point" && !(/^[1-9]\d*$/.test($(element).val())))){
                     alert ($(element.parentNode).find('label').text() + "要填写正整数");
-                    isValid = 1
+                    isValid = 1;
+                    return false;
                 }
             }
         });
@@ -42,12 +53,8 @@ var BaseInfo = Spine.Controller.sub({
             if ($(element).text() === "请选择") {
                 alert ($(element.parentNode.parentNode).find('label').text() + "未选择");
                 isValid = 1;
+                return false;
             };
         });
-        if (isValid == 1) {return}
-        $('.survey_name').find('div>span').text($('#question-name').val());
-        //turn to next page
-        $('#tabs').tabs('enable', 1);
-        $('#tabs').tabs('option', 'active', 1 );
     }
 });

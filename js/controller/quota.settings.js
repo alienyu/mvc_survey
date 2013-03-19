ebd9494c64e9cb513ef6ae6091e85bb6cf5aecd2
alienyu <alienyu@foxmail.com>
2013-03-19 16:35:04 +0800

    add quota template

--- js/controller/quota.settings.js
+++ js/controller/quota.settings.js
@@ -1,6 +1,10 @@
 var QuotaSettings = Spine.Controller.sub({
     template: function () {
-        return $("#quota-settings-template").tmpl();
+        return $("#quota-template").tmpl();
+    },
+
+    events: {
+        "click  #add_quota" : "addQuota"
     },
     show: function () {
         this.el.html(this.template());
@@ -9,6 +13,21 @@ var QuotaSettings = Spine.Controller.sub({
         //show template
         this.show();
         //init widgets
-        
+    },
+
+    addQuota: function() {
+        $('#quota_setting').empty();
+        $('#quota-setting-template').tmpl().appendTo($('#quota_setting'));
+        $(surveyInstance.questions).each(function(i,e) {
+            $('#quota-question-list-template').tmpl({"index":i+1,"question":e.description}).appendTo($('#quota_questions'));
+            if (i==0) {
+                $(e.options).each(function(i,e) {
+                    var option = "<option value=" + i + ">" + e.index + "." + e.content + "</option>";
+                    var answer = "<option value='0'>回答</option><option value='1'>不回答</option>"
+                    $('#quota_question_options').append(option);
+                    $('#is_answer').append(answer);
+                })
+            }
+        })
     }
 });

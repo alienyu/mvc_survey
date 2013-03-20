b6cefe5daa1260e6fa90b4258e3c78a762c080df
Jack Ma <supermaty@126.com>
2013-03-20 11:23:27 +0800

    add logic to logicList implement
    add logic to logicList implement and modify logic object structor on map
    property

--- js/controller/logic.settings.js
+++ js/controller/logic.settings.js
@@ -27,7 +27,7 @@ var LogicSettings = Spine.Controller.sub({
         this.logic = {
             logicName: "",
             logicType: "",
-            map: [],
+            map: {},
             action: {
                 type : "",
                 queN : "",
@@ -36,12 +36,12 @@ var LogicSettings = Spine.Controller.sub({
         };
     },
     _initOptionList: function (e) {
-        //TODO: refactor here
         var index =$(e.target).val();
         console.log(surveyInstance.questions[index].type);
         switch(surveyInstance.questions[index].type) {
             case "single-select" :
             case "multi-select" :
+                //TODO: refactor here
                 $("#condition_questionOption").empty().append("<option>全部</option>");
                 $(surveyInstance.questions[index].options).each(function (index, element){
                     $("#condition_questionOption").append("<option value=" + index + ">" + element.index + element.content + "</option>");
@@ -58,25 +58,33 @@ var LogicSettings = Spine.Controller.sub({
     },
     _initQuestionList: function () {
         $(surveyInstance.questions).each(function (index, element) {
+            //TODO: refactor here
             var optionStr = "<option value=" + index + ">" + index + element.description + "</option>";
             $("#questionList").append(optionStr);
             $("#action_question").append(optionStr);
         });
     },
     renderLogicList: function () {
-//        $("#logicList").empty();
-//        $(surveyInstance.logicList).each(function(index, element) {
-//
-//        });
+        //TODO: refactor here
+        $("#logicList").empty();
+        $(surveyInstance.logicList).each(function(index, element) {
+            if( index % 2 === 0){
+                $("#logicList").append("<li>" + element.logicName + "<a href='#' class='delete'></a></li>");
+            } else {
+                $("#logicList").append("<li class='color'>" + element.logicName + "<a href='#' class='delete'></a></li>");
+            }
+        });
     },
     addCondition: function () {
-        this.logic.map.push({
-            questionIndex : $("#questionList").val(),
-            questionOption : [($("#condition_type").val() === "0" ? "-" : "") + parseInt($("#condition_questionOption").val())]
-        });
-        $("#conditions").val(JSON.stringify(this.condition.map));
+        if(!this.logic.map[ $("#questionList").val()]){
+            this.logic.map[ $("#questionList").val()] = [];
+        }
+        this.logic.map[$("#questionList").val()].push(($("#condition_type").val() === "0" ? "-" : "") + parseInt($("#condition_questionOption").val()));
+        //TODO: need confirm here
+        $("#conditions").val(JSON.stringify(this.logic.map));
     },
     addLogic: function () {
+        this.logic.logicName = $("#logic_name").val();
         this.logic.action.type = $("#actionType").val();
         this.logic.action.queN = $("#action_question").val();
         this.logic.action.optN = "";

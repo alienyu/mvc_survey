092912767350a553f28881e46f0ec51aa9263edf
Jack Ma <supermaty@126.com>
2013-03-20 10:35:07 +0800

    logic setting implement
    add condition and action to logic

--- js/controller/logic.settings.js
+++ js/controller/logic.settings.js
@@ -5,9 +5,9 @@ var LogicSettings = Spine.Controller.sub({
     events: {
         "click .delete": "logicDelete",
         "click #init_editor": "initEditor",
+        "change #questionList": "_initOptionList",
         "click #addCondition": "addCondition",
-        "click #save_logic": "addLogic",
-        "change #questionList": "_initOptionList"
+        "click #save_logic": "addLogic"
     },
     show: function () {
         this.el.html(this.template());
@@ -16,48 +16,33 @@ var LogicSettings = Spine.Controller.sub({
         //show template
         this.show();
         //init widgets
-        this.condition = {
-            map: [],
-            action: {
-                type : "",
-                queN : "",
-                optN : ""}
-        };
-    },
-    logicDelete: function (e) {
-        var index = $(e.target).parents("li").index();
-        console.log(index);
+        this._initLogicObj();
+        this.renderLogicList();
     },
     initEditor: function () {
         $("#logic-settings-container").html($("#logic-settings-edit-template").tmpl());
         this._initQuestionList();
     },
-    addCondition: function () {
-        this.condition.map.push({
-            questionIndex : $("#questionList").val(),
-            questionOption : [($("#condition_type").val() === "0" ? "-" : "") + parseInt($("#condition_questionOption").val())]
-        });
-        $("#conditions").val(JSON.stringify(this.condition.map));
-    },
-    addLogic: function () {
-        var logicModel = {
+    _initLogicObj: function () {
+        this.logic = {
+            logicName: "",
+            logicType: "",
+            map: [],
+            action: {
+                type : "",
+                queN : "",
+                optN : ""
+            }
         };
-        console.log("addLogic");
-    },
-    _initQuestionList: function () {
-        $(surveyInstance.questions).each(function (index, element) {
-            var optionStr = "<option value=" + index + ">" + index + element.description + "</option>";
-            $("#questionList").append(optionStr);
-            $("#action_question").append(optionStr);
-        });
     },
     _initOptionList: function (e) {
+        //TODO: refactor here
         var index =$(e.target).val();
         console.log(surveyInstance.questions[index].type);
         switch(surveyInstance.questions[index].type) {
             case "single-select" :
             case "multi-select" :
-                $("#condition_questionOption").empty().append("<option value='-1'>全部</option>");
+                $("#condition_questionOption").empty().append("<option>全部</option>");
                 $(surveyInstance.questions[index].options).each(function (index, element){
                     $("#condition_questionOption").append("<option value=" + index + ">" + element.index + element.content + "</option>");
                 });
@@ -70,5 +55,39 @@ var LogicSettings = Spine.Controller.sub({
                 $("#condition_type")._hide();
                 break;
         }
+    },
+    _initQuestionList: function () {
+        $(surveyInstance.questions).each(function (index, element) {
+            var optionStr = "<option value=" + index + ">" + index + element.description + "</option>";
+            $("#questionList").append(optionStr);
+            $("#action_question").append(optionStr);
+        });
+    },
+    renderLogicList: function () {
+//        $("#logicList").empty();
+//        $(surveyInstance.logicList).each(function(index, element) {
+//
+//        });
+    },
+    addCondition: function () {
+        this.logic.map.push({
+            questionIndex : $("#questionList").val(),
+            questionOption : [($("#condition_type").val() === "0" ? "-" : "") + parseInt($("#condition_questionOption").val())]
+        });
+        $("#conditions").val(JSON.stringify(this.condition.map));
+    },
+    addLogic: function () {
+        this.logic.action.type = $("#actionType").val();
+        this.logic.action.queN = $("#action_question").val();
+        this.logic.action.optN = "";
+        //console.log(this.condition);
+        surveyInstance.logicList.push(this.logic);
+        this._initLogicObj();
+        this.renderLogicList();
+        $("#logic-settings-container").empty();
+    },
+    logicDelete: function (e) {
+        var index = $(e.target).parents("li").index();
+        console.log(index);
     }
 });
